e4e6a5b573c1fdc724828c74568d3749aa411823
bibo <abanoubharby@gmail.com>
2022-11-04 15:38:23 +0100

    make navbar responsiv

---
+++ src/components/NavLinks.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import Logout from './Logout'
+const NavLinks = (props) => {
+    const { currentUser, isAuthenticated } = useSelector(state => state.auth)
+
+  return (
+      <p className="navbar-links">
+          {!isAuthenticated?
+            <>
+              <Link to='/register' className='navbar-links-signup'>Sign Up</Link>
+              <Link to='/login' >Login</Link><span> | </span>
+            </>
+          :  
+            <>
+              <Link to="/profile" >{currentUser.email}</Link><span> | </span>
+              <Logout /><span> | </span>
+            </>}
+          <Link to='VFDs' >For Doctors</Link><span> | </span>
+          <Link to='/contactUs' >Contact Us</Link><span> | </span>
+          <Link to='/Lang' >عربي</Link><span> | </span>
+          <span>&#x1f1ea;&#x1f1ec;</span>
+        </p>
+  )
+}
+
+export default NavLinks
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,34 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import Logout from './Logout'
 
 import logo from '../assets/3yada-online-low-resolution-logo-white-on-transparent-background.png'
+import NavLinks from './NavLinks'
 
 
 const Navbar = () => {
-  const { currentUser, isAuthenticated } = useSelector(state => state.auth)
+  const [ isNavbarOpen, setisNavbarOpen ] = useState (false);
 
   return (
-    <div className='navbar'>
-      <Link className='navbar-logo' to='/'>
-        <img  className='navbar-logo-img' src={logo} alt="white logo with dotcom" />
-        <span>3yada Online</span>
-      </Link>
-      <p className='navbar-links'>
-          {!isAuthenticated?
-            <>
-              <Link to='/register' className='custom_a navbar-links-signup'>Sign Up</Link>
-              <Link className='custom_a' to='/login' >Login</Link><span> | </span>
-            </>
-          :  
-            <>
-              <Link to="/profile" className="custom_a">{currentUser.email}</Link><span> | </span>
-              <Logout /><span> | </span>
-            </>}
-          <Link className='custom_a' to='VFDs' >For Doctors</Link><span> | </span>
-          <Link className='custom_a' to='/contactUs' >Contact Us</Link><span> | </span>
-          <Link className='custom_a' to='/Lang' >عربي</Link><span> | </span>
-          <span>&#x1f1ea;&#x1f1ec;</span>
-      </p>
-    </div>
+    <>
+      <div className='navbar'>
+        <Link className='navbar-logo' to='/'>
+          <img  className='navbar-logo-img' src={logo} alt="white logo with dotcom" />
+          <span>3yada Online</span>
+        </Link>
+        <div className="navLinks">
+          <NavLinks />    
+        </div>
+        {!isNavbarOpen&& <i className='fa fa-bars mobile-navbar' onClick={()=>setisNavbarOpen(true)}/>}    
+      </div>
+      <div className='mobile-navbar'>
+        {isNavbarOpen&&
+        <div className='sidebar'>
+          <i className='fa fa-times' onClick={()=>setisNavbarOpen(false)} />
+          <NavLinks />
+        </div> 
+        }
+      </div>
+    </>
   )
 }
 
