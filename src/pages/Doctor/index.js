32085239a4d5fee705835e822e7c3cb8a18e31f0
bibo <abanoubharby@gmail.com>
2022-11-26 19:44:10 +0100

    add blank doctor page

---
+++ src/pages/Doctor/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Doctor = () => {
+    
+    return (
+        <div className="doctor-page">
+            <h1>Welcome to Doctor Page</h1>
+        </div>
+    )
+}
